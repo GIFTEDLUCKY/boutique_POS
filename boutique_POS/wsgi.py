66be4d9307031f5e1297dda8048bb4a2940253aa
GLIMS-PRO <glimsprosg@gmail.com>
2025-08-06 02:15:07 +0000

    Your commit message

--- boutique_POS/wsgi.py
+++ boutique_POS/wsgi.py
@@ -1,11 +1,18 @@
 import os
 import sys
 
-path = '/home/giftedlucky/boutique_POS/boutique_POS'
-if path not in sys.path:
-    sys.path.append(path)
+# Add your project directory to the Python path
+project_home = '/home/giftedlucky/boutique_POS/boutique_POS'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boutique_POS.settings")
+# Activate your virtualenv (optional but recommended)
+activate_this = '/home/giftedlucky/boutique_POS/boutique_POS/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Set the Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boutique_POS.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
