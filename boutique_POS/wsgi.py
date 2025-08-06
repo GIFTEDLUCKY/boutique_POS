5a0b182c57fa85c33bbeee78583a2a05e44bdeb4
GLIMS-PRO <glimsprosg@gmail.com>
2025-08-06 20:57:59 +0000

    Deployment fixes

--- boutique_POS/wsgi.py
+++ boutique_POS/wsgi.py
@@ -1,10 +1,15 @@
 import os
 import sys
 
-# Set project base directory
-project_home = '/home/giftedlucky/boutique_POS'
+# Correct project directory - includes the inner boutique_POS folder
+project_home = '/home/giftedlucky/boutique_POS/boutique_POS'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
+
+# Activate your virtualenv
+activate_this = '/home/giftedlucky/boutique_POS/boutique_POS/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
 
 # Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boutique_POS.settings')
@@ -13,6 +18,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boutique_POS.settings')
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# Optional: Enable WhiteNoise for static files
+# Serve static files with WhiteNoise
 from whitenoise import WhiteNoise
 application = WhiteNoise(application, root=os.path.join(project_home, 'staticfiles'))
