39b8ecd5384855e2ccf8389507871c5de2c20205
GLIMS-PRO <glimsprosg@gmail.com>
2025-08-06 02:25:18 +0000

    Your commit message

--- boutique_POS/wsgi.py
+++ boutique_POS/wsgi.py
@@ -1,22 +1,18 @@
 import os
 import sys
-from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
-# Add your project directory to the Python path
-project_home = '/home/giftedlucky/boutique_POS/boutique_POS'
+# Set project base directory
+project_home = '/home/giftedlucky/boutique_POS'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
-
-# Activate your virtualenv (optional but recommended)
-activate_this = '/home/giftedlucky/boutique_POS/boutique_POS/venv/bin/activate_this.py'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
 
 # Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boutique_POS.settings')
 
+# Set up Django application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root='/home/giftedlucky/boutique_POS/boutique_POS/staticfiles')
+# Optional: Enable WhiteNoise for static files
+from whitenoise import WhiteNoise
+application = WhiteNoise(application, root=os.path.join(project_home, 'staticfiles'))
