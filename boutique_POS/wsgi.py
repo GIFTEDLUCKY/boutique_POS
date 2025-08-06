855d2c0650aa1d0812efcfafb5efc2c1ec91674e
GLIMS-PRO <glimsprosg@gmail.com>
2025-08-06 22:07:30 +0000

    Configured wsgi.py for PythonAnywhere deployment

--- boutique_POS/wsgi.py
+++ boutique_POS/wsgi.py
@@ -1,23 +1,26 @@
 import os
 import sys
 
-# Add your project directory to the sys.path
-project_home = '/home/giftedlucky/boutique_POS'
+# Tell Django we are on PythonAnywhere
+os.environ['PYTHONANYWHERE'] = 'true'
+
+# Set correct project directory
+project_home = '/home/giftedlucky/boutique_POS/boutique_POS'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-# Activate your virtual environment
-activate_this = '/home/giftedlucky/boutique_POS/venv/bin/activate_this.py'
-with open(activate_this) as f:
-    exec(f.read(), dict(__file__=activate_this))
+# Activate your virtualenv
+activate_this = '/home/giftedlucky/boutique_POS/boutique_POS/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
 
-# Set the Django settings module
+# Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boutique_POS.settings')
 
-# Get WSGI application
+# Load the Django application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# Optional: Static files (if using WhiteNoise)
+# Serve static files with WhiteNoise
 from whitenoise import WhiteNoise
 application = WhiteNoise(application, root=os.path.join(project_home, 'staticfiles'))
