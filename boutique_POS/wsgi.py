f7cac265137d4c588a74034c1fea190c667abebb
GLIMS-PRO <glimsprosg@gmail.com>
2025-08-06 01:33:24 +0000

    Your commit message

--- boutique_POS/wsgi.py
+++ boutique_POS/wsgi.py
@@ -1,17 +1,18 @@
-"""
-WSGI config for multi_store_pos project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
+import sys
 import os
 
+# Add your project directory to the sys.path
+project_home = '/home/giftedlucky/boutique_POS'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Set the Django settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'boutique_POS.settings'
+
+# Activate virtualenv if using one (skip this if you don’t have a venv)
+activate_this = '/home/giftedlucky/.virtualenvs/your_venv_name/bin/activate_this.py'
+exec(open(activate_this).read(), dict(__file__=activate_this))
+
+# Get the WSGI application
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boutique_POS.settings')
-
-
 application = get_wsgi_application()
